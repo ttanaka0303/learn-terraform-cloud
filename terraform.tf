5be31fa2877450fec77a56063e396b5470415c22
Tomohiro Tanaka <ttanaka@aras.com>
2023-02-14 13:59:14 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+*/
   cloud {
-    organization = "organization-name"
+    organization = "Tanaka-Corp"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
